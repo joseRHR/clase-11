cc2226fbddf1fac63afee3f86a03bf9da51d348c
Laboratorio 05 Sede VES <Ves-Lab05@certus.edu.pe>
2024-09-19 10:08:00 -0500

    Clase 11

---
+++ js/libreria.js
@@ -0,0 +1,9 @@
+//Muestra un mensaje
+alert('Buenos días programadores');
+function mensaje(){
+    //Bloque de código que realiza una actividad
+    alert("INSTITUTO CERTUS");
+}
+function presentar(){
+    alert('Lenguajes para el desarrollo y soporte de Sistemas');
+}
